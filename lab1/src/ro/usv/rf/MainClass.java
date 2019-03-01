ba5ae9134d1ea6722e128cd0e63809e99ddb9758
ioanagherasim3 <48050967+ioanagherasim3@users.noreply.github.com>
2019-03-01 12:25:12 +0200

    Add files via upload

---
+++ lab1/src/ro/usv/rf/MainClass.java
@@ -0,0 +1,48 @@
+package ro.usv.rf;
+
+public class MainClass {
+
+	private static double[][] normalizeLearningSet(double[][] learningSet) {
+		double[][] normalizedLearningSet = new double[learningSet.length][];
+		
+		double[] max=new double [learningSet[0].length];
+		double[] min=new double [learningSet[0].length];
+		// .. enter your code here
+		for(int i=0;i<learningSet[0].length;i++)
+		{
+			max[i]=learningSet[0][i];
+			min[i]=learningSet[0][i];
+		}
+		
+		for(int row=1;row<learningSet.length;row++)
+		{
+			for(int column=0;column<learningSet.length;column++)
+			{
+				if(learningSet[row][column]>max[column])
+				{
+					max[column]=learningSet[row][column];
+				}
+				if(learningSet[row][column]<min[column])
+				{
+					min[column]=learningSet[row][column];
+				}
+			}
+		}
+		for(int row=0;row<learningSet.length;row++)
+		{
+			normalizedLearningSet[row]=new double[learningSet[0].length];
+			for(int column=0;column<learningSet[0].length;column++)
+			{
+				normalizedLearningSet[row][column]=(learningSet[row][column]-min[column])/(max[column]-min[column]);
+			}
+		}
+		
+		return normalizedLearningSet;
+	}
+	
+	public static void main(String[] args) {
+		double[][] learningSet = FileUtils.readLearningSetFromFile("C:\\Users\\Student\\eclipse-workspace\\lab1\\in.txt");
+		FileUtils.writeLearningSetToFile("C:\\Users\\Student\\eclipse-workspace\\lab1\\out.txt",
+		normalizeLearningSet(learningSet));
+		}
+}
